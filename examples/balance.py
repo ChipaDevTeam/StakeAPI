78dc0ea96a53a8dc610a6023148852425957923d
Vigo Walker <vigopaul05@gmail.com>
2026-04-04 11:48:59 +0200

    Add user_agent support to balance example for customizable headers

--- examples/balance.py
+++ examples/balance.py
@@ -10,7 +10,8 @@ async def main():
     access_token = os.getenv("STAKE_ACCESS_TOKEN")
     session_cookie = os.getenv("STAKE_SESSION_COOKIE")
     cf_clearance = os.getenv("STAKE_CF_CLEARANCE")
-    async with StakeAPI(access_token=access_token, session_cookie=session_cookie, cf_clearance=cf_clearance) as client:
+    user_agent = os.getenv("STAKE_USER_AGENT")
+    async with StakeAPI(access_token=access_token, session_cookie=session_cookie, cf_clearance=cf_clearance, user_agent=user_agent) as client:
         
         # 1. Get your balance
         balance = await client.get_user_balance()
